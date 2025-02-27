c066857f3fb69878f936e659d93edfc2dac1ef53
Gorsky Kirill <kirillgorskiy@gmail.com>
2025-02-27 16:48:04 +0300

    add inf generator + trapezoid method

--- src/generators/inf.cpp
+++ src/generators/inf.cpp
@@ -7,14 +7,12 @@ int main()
     printf("%d\n", 3);
     
     BodyState bodies[3] = {
-        BodyState { {-1, 0, 0}, {0, 0, -1} },
-        BodyState { {0, 0, 0}, {sqrt(2)/2, 0, sqrt(2)/2} },
-        BodyState { {1, 0, 0}, {0, 0, 1} }
+        BodyState { {-1, 0, 0}, {0.347111, 0, 0.532728} },
+        BodyState { {0, 0, 0}, {-0.694222, 0, -1.065456} },
+        BodyState { {1, 0, 0}, {0.347111, 0, 0.532728} }
     };
 
     for (int i = 0; i < 3; i++) {
-        //bodies[i].velocity *= 0.5;
-
         printf("1 %lf %lf %lf %lf %lf %lf\n",
             bodies[i].position.x, bodies[i].position.y, bodies[i].position.z,
             bodies[i].velocity.x, bodies[i].velocity.y, bodies[i].velocity.z
